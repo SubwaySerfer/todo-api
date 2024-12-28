f8fbaa95586b7b11135643fc49dfc35b091b875d
Aleksander Afanasyev <afonia.marcii@gmail.com>
2024-12-28 16:51:17 +0100

    add CORS support and update server port; modify HTTP request examples for consistency

--- go.mod
+++ go.mod
@@ -16,6 +16,7 @@ require (
 	github.com/cloudwego/iasm v0.2.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.7 // indirect
+	github.com/gin-contrib/cors v1.7.3 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
@@ -37,7 +38,7 @@ require (
 	golang.org/x/net v0.33.0 // indirect
 	golang.org/x/sys v0.28.0 // indirect
 	golang.org/x/text v0.21.0 // indirect
-	google.golang.org/protobuf v1.36.0 // indirect
+	google.golang.org/protobuf v1.36.1 // indirect
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,19 +1,41 @@
 package main
 
 import (
-    "todo-api/routes"
-		"todo-api/db"
-    "github.com/gin-gonic/gin"
+	"time"
+	"todo-api/db"
+	"todo-api/routes"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
+
 	db.InitDB()
 	defer db.CloseDB()
 
 	db.CreateTasksTable(db.DB)
-  r := gin.Default()
+	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"http://localhost:5500"},        // Разрешённые домены
+		AllowMethods: []string{"GET", "POST", "PUT", "DELETE"}, // Разрешённые методы
+		AllowHeaders: []string{
+			"Origin",
+			"Content-Type",
+			"Authorization",
+			"HX-Request",
+			"HX-Target",
+			"HX-Trigger",
+			"HX-Trigger-Name",
+			"HX-Current-URL", // Добавляем hx-current-url
+		},
+		ExposeHeaders:    []string{"Content-Length"}, // Заголовки, видимые клиенту
+		AllowCredentials: true,                       // Разрешение на передачу куков
+		MaxAge:           12 * time.Hour,             // Кэширование CORS настроек
+	}))
 
 	routes.SetupRoutes(r)
 
-  r.Run(":8080")
+	r.Run(":8088")
 }
